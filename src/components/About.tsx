9b4b1004f43aef8e261886ef144a5c741eb13d5a
Ahmed Reda <ahmed765re@gmail.com>
2026-04-18 14:46:23 +0200

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -26,7 +26,7 @@ const About = () => {
   ];
 
   return (
-    <section id="about" className="ppy-12 md:py-20 px-4 md:px-0 relative overflow-hidden">
+    <section id="about" className="ppy-12 md:py-20 mt-2 px-4 md:px-0 relative overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" ref={ref}>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
@@ -35,7 +35,7 @@ const About = () => {
           className="relative order-2 md:order-1"
         >
          
-          <h2 className="text-center  mb-10 md:mb-16">
+          <h2 className="text-center   mb-4 md:mb-4">
             <span className="bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent text-2xl sm:text-3xl md:text-5xl font-bold mb-4 text-center">
               About Me
             </span>
